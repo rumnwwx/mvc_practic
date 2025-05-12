5ba0933fdeb95ce6d625ad71dbce4241762d8196
rumnwwx <elchaninapolina@gmail.com>
2025-05-12 13:05:28 +0700

    create auth

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -21,3 +21,10 @@ function getConfigs(string $path = DIR_CONFIG): array
 require_once __DIR__ . '/../routes/web.php';
 
 return new Src\Application(new Src\Settings(getConfigs()));
+
+function app() {
+    global $app;
+    return $app;
+}
+
+return $app;
